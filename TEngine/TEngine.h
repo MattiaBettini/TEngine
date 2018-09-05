128792a5fb289def0dd9523f3e51ca9b8ebade13
Alessio Greco <alxeyesoul@live.com>
2018-09-05 17:26:39 +0200

    engine initialization related types dll export

--- TEngine/TEngine.h
+++ TEngine/TEngine.h
@@ -11,4 +11,27 @@ using namespace std;
 	#define TEngineAPI _declspec(dllimport)
 #endif
 
-extern "C" string TEngineAPI GetTestString();
+//extern "C" string TEngineAPI GetTestString();
+
+namespace TEngine
+{
+	enum RenderSystem
+	{
+		OpenGl = 1,
+		Vulkan = 2
+	};
+
+	class TEngineAPI IEngine
+	{
+	public:
+		virtual ~IEngine() {}
+		virtual void Run() = 0;
+	};
+
+	class TEngineAPI TEngineFactory
+	{
+	public:
+		static IEngine* GetEngine(int iWidth, int iHeight, const string& wTitle, RenderSystem eRenderSubSystem);
+		static void ReleaseEngine(IEngine* pEngine);
+	};
+}
